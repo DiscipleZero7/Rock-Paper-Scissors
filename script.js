c225d3a05067ca581336272588e09fa6f60e63cf
Kyle Hines <disciplezero7@gmail.com>
2025-10-28 14:01:36 -0500

    Add Logic to play a single round and add score

--- script.js
+++ script.js
@@ -1,18 +1,83 @@
+let humanScore = 0;
+let computerScore = 0;
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
 function getComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3);
     
     switch(randomNumber) {
         case 0:
-            return "Rock";
+            return "rock";
         
         case 1:
-            return "Paper";
+            return "paper";
 
         case 2:
-            return "Scissors";
+            return "scissors";
     }
 }
 
 function getHumanChoice() {
-    return prompt("Rock, Paper, or Scissors?");
+    const answer = prompt("Rock, Paper, or Scissors?");
+    return answer.toLowerCase();
 }
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === "rock") {
+        switch(computerChoice) {
+            case "rock":
+                console.log("It's a draw.")
+                return;
+            
+            case "paper":
+                console.log("You lose! Paper beats Rock");
+                computerScore++;
+                return;
+
+            case "scissors":
+                console.log("You win! Rock beats Scissors.");
+                humanScore++;
+                return
+        } 
+    }
+
+    if (humanChoice === "paper") {
+        switch(computerChoice) {
+            case "rock":
+                console.log("You win! Paper beats Rock");
+                humanScore++;
+                return;
+            
+            case "paper":
+                console.log("It's a draw.")
+                return;
+
+            case "scissors":
+                console.log("You lose! Scissors beats Paper");
+                computerScore++;
+                return;
+        } 
+    }
+
+    if (humanChoice === "scissors") {
+        switch(computerChoice) {
+            case "rock":
+                console.log("You lose! Rock beats Scissors");
+                computerScore++;
+                return;
+
+            case "paper":
+                console.log("You win! Scissors beats paper.");
+                humanScore++;
+                return
+
+            case "scissors":
+                console.log("It's a draw.");
+                return
+        } 
+    } 
+}
+
+playRound(humanSelection, computerSelection);
